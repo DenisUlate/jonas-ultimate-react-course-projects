8a71b2f4d3d4001b316e978f5f885cd4d5139a07
Denis Ulate <dennisjose.us@gmail.com>
2024-06-06 13:13:57 -0600

    Final Version 07-exercise-accordion

--- 07-exercise-accordion/src/App.jsx
+++ 07-exercise-accordion/src/App.jsx
@@ -24,6 +24,8 @@ function App() {
 }
 
 function Accordion({ data }) {
+	const [curOpen, setCurOpen] = useState(null);
+
 	return (
 		<div className="accordion">
 			{data.map((item, index) => (
@@ -32,24 +34,29 @@ function Accordion({ data }) {
 					num={index + 1}
 					title={item.title}
 					text={item.text}
-				/>
+					curOpen={curOpen}
+					onOpen={setCurOpen}>
+					{item.text}
+				</AccordionItem>
 			))}
 		</div>
 	);
 }
 
-function AccordionItem({ num, title, text }) {
-	const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+	// const [isOpen, setIsOpen] = useState(false);
+	const isOpen = num === curOpen;
 
 	function handleToggle() {
-		setIsOpen(!isOpen);
+		onOpen(isOpen ? null : num);
+		// setIsOpen(!isOpen);
 	}
 	return (
 		<div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
 			<p className="number">{num}</p>
 			<p className="title">{title}</p>
 			<p className="icon">{isOpen ? "-" : "+"}</p>
-			{isOpen && <div className="content-box">{text}</div>}
+			{isOpen && <div className="content-box">{children}</div>}
 		</div>
 	);
 }
